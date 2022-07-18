7a9399f154931c4b20282eb2b3a8e6f7ddf7bf3a
Capsonchik <Serega00790@mail.ru>
2022-07-18 14:28:50 +0300

    add some mocks OFFERS

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,36 @@
+import { ObjectReview } from "../types/object"
+
+export const objects: ObjectReview[] = [
+  {
+    id: 1,
+    name: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartament',
+    mainPhoto: 'img',
+    rating: 4,
+    price: 120,
+  },
+  {
+    id: 2,
+    name: 'Wood and stone place',
+    type: 'Private room',
+    mainPhoto: 'img',
+    rating: 4,
+    price: 80,
+  },
+  {
+    id: 3,
+    name: 'Wood and stone place',
+    type: 'Private room',
+    mainPhoto: 'img',
+    rating: 4,
+    price: 80,
+  },
+  {
+    id: 4,
+    name: 'Wood and stone place',
+    type: 'Private room',
+    mainPhoto: 'img',
+    rating: 4,
+    price: 80,
+  },
+]
---
+++ project/src/types/object.ts
@@ -0,0 +1,8 @@
+export type ObjectReview = {
+  id: number,
+  name: string,
+  type: string,
+  mainPhoto: string,
+  rating: number,
+  price: number,
+};
